68d0d8d431a3529d2b94b686581e3f5664c82243
matthias <matthias@mucqmaws092>
2020-02-17 13:49:02 +0100

    increase/decrease clock speed

--- arduino-clock.ino
+++ arduino-clock.ino
@@ -7,11 +7,28 @@ bool CLOCK = false;
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(PIN_CLOCK, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    char c = Serial.read();
+    switch (c) {
+      case '+':
+        // increase clock speed (+10%)
+        clock_delay *= 0.9;
+        Serial.println(clock_delay);
+        break;
+
+      case '-':
+        // decrease clock speed (-10%)
+        clock_delay *= 1.1;
+        Serial.println(clock_delay);
+        break;
+    }
+  }
   digitalWrite(PIN_CLOCK, CLOCK ? HIGH : LOW);
   delay(clock_delay);
 
